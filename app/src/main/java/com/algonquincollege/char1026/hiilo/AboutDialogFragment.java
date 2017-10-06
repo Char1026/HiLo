c173bc6f87ebdb5bbe9cc693eeb66e9c1f430517
Char1026 <tylercharlebois@Tylers-MacBook-Pro.local>
2017-10-05 23:54:21 -0400

    HiLo

---
+++ app/src/main/java/com/algonquincollege/char1026/hiilo/AboutDialogFragment.java
@@ -0,0 +1,28 @@
+package com.algonquincollege.char1026.hiilo;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.os.Bundle;
+
+/**
+ * Created by tylercharlebois on 2017-10-05.
+ */
+
+public class AboutDialogFragment extends DialogFragment {
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle(R.string.action_about)
+                .setMessage(R.string.author)
+                .setCancelable(false)
+                .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+        return builder.create();
+    }
+}
